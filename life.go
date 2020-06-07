3f11f32c543258ac3224a234017f4ca67fd756a6
r3volved <32720839+r3volved@users.noreply.github.com>
2020-06-07 17:57:55 -0400

    Add files via upload

--- life.go
+++ life.go
@@ -1,73 +1,164 @@
 package main
 
 import (
-	"time"
 	"fmt"
+	"math/rand"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
 )
 
-func printer ( board *[30][30]int ) {
-	print("\033[H\033[2J")
-	for _, col := range board {
-		line := ""
-		for _, val := range col {
-			if val != 0 {
-				line += "▉"	
-			} else {
-				line += " "
-			}
-		}
-		fmt.Println(line)
-	}
-	time.Sleep(time.Second/10)
+func clearScreen() {
+	c := exec.Command("clear")
+	c.Stdout = os.Stdout
+	c.Run()
 }
 
-func checkSurround( board [30][30]int, row, col int ) int {
+type Board struct {
+	content [10][30]int
+}
+
+func (b Board) rows() [10][30]int {
+	return b.content
+}
+func (b Board) cols(row int) [30]int {
+	return b.content[row]
+}
+func (b Board) val(row, col int) int {
+	return b.content[row][col]
+}
+
+func printer(board *Board, iterations int, looped int) {
+	report := ""
+	report += strings.Repeat("-", len(board.content[0])+2) + "\n"
+	for _, col := range board.rows() {
+		report += "|"
+		for _, val := range col {
+			if val == 0 {
+				report += " "
+			} else {
+				report += "▮"
+			}
+		}
+		report += "|\n"
+	}
+	report += strings.Repeat("-", len(board.content[0])+2) + "\n"
+	report += "Iterations : " + strconv.Itoa(iterations) + "\n"
+	if looped > 0 {
+		report += "Looped : " + strconv.Itoa(looped) + "\n"
+	}
+
+	clearScreen()
+	fmt.Println(report)
+}
+
+func checkSurround(board *Board, row, col int) int {
 	neighbours := 0
-	for r := row-1; r <= row+1; r++ {
-		if r < 0 || r >= len(board) { continue }
-		for c := col-1; c <= col+1; c++ {
-			if c < 0 || c >= len(board[r]) { continue }
-			if col == c && row == r { continue }
-			if board[r][c] != 0 { neighbours++ }
+	spaces := [3]int{-1, 0, 1}
+	rowlength := len(board.rows())
+	collength := len(board.cols(0))
+	for _, r := range spaces {
+		if r+row < 0 {
+			r += rowlength
+		}
+		if r+row >= rowlength {
+			r -= rowlength
+		}
+		for _, c := range spaces {
+			if c == 0 && r == 0 {
+				continue
+			}
+			if c+col < 0 {
+				c += collength
+			}
+			if c+col >= collength {
+				c -= collength
+			}
+			if board.val(r+row, c+col) > 0 {
+				neighbours++
+			}
 		}
 	}
 	return neighbours
 }
 
-func checkCell( board [30][30]int, row, col int ) int {
-	alive := board[row][col] != 0
-	neighbours := checkSurround( board, row, col )
+func checkCell(board *Board, row, col int) int {
+	alive := board.val(row, col) != 0
+	neighbours := checkSurround(board, row, col)
 
-	if neighbours == 3 { return 1 }	
-	if alive && neighbours == 2 { return 1 }
+	if neighbours == 3 {
+		return 1
+	}
+	if alive && neighbours == 2 {
+		return 1
+	}
 	return 0
 }
 
-func main() {	
-
-	var board [30][30]int
-
-	iterations := 1000
-
-	//Seed the board
-	board[10][10] = 1
-	board[10][11] = 1
-	board[11][9] = 1
-	board[11][10] = 1
-	board[12][10] = 1
-
-	for i := 0; i <= iterations; i++ {
-		printer( &board )
-		var newBoard [30][30]int
-		for row := 0; row < len(board); row++ {
-            for col := 0; col < len(board[row]); col++ {
-                newBoard[row][col] = checkCell( board, row, col )
-            }
-        }
-        board = newBoard
+func step(board *Board) Board {
+	newBoard := *board
+	for row := 0; row < len(board.rows()); row++ {
+		for col := 0; col < len(board.cols(row)); col++ {
+			newBoard.content[row][col] = checkCell(board, row, col)
+		}
 	}
-
+	return newBoard
 }
 
+func seedRandom(board *Board) {
+	for row := 0; row < len(board.rows()); row++ {
+		for col := 0; col < len(board.cols(row)); col++ {
+			rand.Seed(time.Now().UnixNano())
+			board.content[row][col] = rand.Intn(2)
+		}
+	}
+}
 
+func checkHistory(history []Board, board Board) int {
+	found := -1
+	for i, b := range history {
+		if b == board {
+			found = len(history) - i
+			break
+		}
+	}
+	return found - 1
+}
 
+func main() {
+
+	iterations := 0
+	looped := 0
+	var board Board
+	var history []Board
+
+	//Seed the board
+	seedRandom(&board)
+
+	for {
+		printer(&board, iterations, looped)
+
+		if looped == 1 {
+			fmt.Println("Life has stalled out")
+			break
+		} else if looped > 1 {
+			fmt.Println("Life got looped")
+			break
+		}
+
+		looped = checkHistory(history, board)
+
+		board = step(&board)
+
+		history = append(history, board)
+
+		iterations++
+		time.Sleep(time.Second / 20)
+
+	}
+
+	fmt.Println("Game Over")
+
+}
